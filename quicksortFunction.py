c97c2c6b15e677766bd10aa7828369f05761b744
Lara Edwards <anakinedwards9@gmail.com>
2025-04-20 18:23:01 +0100

    Current code as of today, quicksort function completley working but visualization not quite yet

---
+++ quicksortFunction.py
@@ -0,0 +1,59 @@
+from random import shuffle
+import tkinter as tk
+
+window = tk.Tk()
+canWidth,canHeight = 500,500
+canvas = tk.Canvas(window, width=canWidth, height=canHeight, bg='light grey')
+canvas.pack()
+
+values = [i+1 for i in range(500)]
+shuffle(values)
+print(values)
+
+def drawLines(z):
+    for i in range (len(z)):
+        canvas.create_line(i, 500, i, 500-z[i], fill= 'black')
+
+def quicksort(z, start, end, depth = 0):
+    if end - start < 1:
+        return
+    
+    pivot = z[end]
+
+    i, j = start, end
+
+    while i < j:
+        #if depth == 0:
+            #drawLines(z)
+
+        while i < j and z[i] < pivot:
+            i += 1
+        while i < j and z[j] > pivot:
+            j -= 1
+
+        if i < j:
+            z[i], z[j] = z[j], z[i]
+
+            if z[i] < pivot:
+                i += 1
+            if z[j] > pivot:
+                j -= 1
+
+    quicksort(z, start, z.index(pivot) - 1, depth + 1)
+    quicksort(z, z.index(pivot) + 1, end, depth + 1)
+
+    return z
+
+quicksort(values, 0, len(values)-1)
+print(values)
+
+def mainLoop():
+
+    canvas.delete("all")
+
+    drawLines(values)
+
+    window.after(16, mainLoop)
+
+mainLoop()
+window.mainloop()
